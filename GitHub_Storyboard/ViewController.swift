ca19b8537a06ae02d7a6f97e71ec4a53ec7da6db
Dilmurod <karimovd892@gmail.com>
2022-07-12 17:18:12 +0500

    Initial Commit

---
+++ GitHub_Storyboard/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  GitHub_Storyboard
+//
+//  Created by Karimov Dilmurod on 12/07/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
